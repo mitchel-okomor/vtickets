a5fe2e8977c2f775bfc623f618258e7e4f563eb6
salesruby <administrator@salesruby.com>
2021-11-30 21:51:32 +0100

    making phone required

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   firstname: { type: String, required: true },
   lastname: { type: String, required: true },
   username: String,
-  phone: String,
+  phone: { type: String, require: true },
   role: { type: String, default: 'user' },
   email: { type: String, index: true, unique: true, required: true },
   password: { type: String, required: true },
